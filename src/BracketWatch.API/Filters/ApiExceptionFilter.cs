a8e4af5ab207e44ddb1ef56c53628baa3aa4aae0
Gurtygurt98 <81448848+Gurtygurt98@users.noreply.github.com>
2025-07-17 22:52:59 -0400

    Initial Commit

---
+++ src/BracketWatch.API/Filters/ApiExceptionFilter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+namespace BracketWatch.API.Filters
+{
+    public class ApiExceptionFilter : ExceptionFilterAttribute
+    {
+        private readonly ILogger<ApiExceptionFilter> _logger;
+
+        public ApiExceptionFilter(ILogger<ApiExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
